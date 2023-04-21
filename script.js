f3eb3722a5caac98bcd6f5bc57fcbd877d092a0d
chhotu marandi <cmarandi29@gmail.com>
2023-04-22 00:05:39 +0530

    close button is not working

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
+
 const modelContent = document.querySelector(".model-content");
 
-const modelFocus = document.querySelector(".model-focus");
-const closeBtn = document.querySelector(".model-focus i");
+const closeButton = document.querySelector(".close-button");
 
-console.log(closeBtn)
+const openModel = document.querySelector(".open-model")
 
-const viewModelContent = () => {
- modelFocus.classList.add("show-focus");
-}
+modelContent.addEventListener('click', () => {
+ openModel.classList.add("showContent");
+});
 
-const closebtnFunc = () => {
- if(modelFocus.classList.contains('show-focus')) {
-  modelFocus.classList.remove('show-focus');
-  modelFocus.classList.add("hide-focus");
- }
-
-}
-
-
-modelContent.addEventListener('click', viewModelContent);
-closeBtn.addEventListener("click", closebtnFunc);
+closeButton.addEventListener("click", () => {
+ openModel.classList.remove("showContent");
+});
